d4269ea26164ae0f701c9c9cab67d13b876b28dc
Madelynn Bronson <mjbronso@ucsc.edu>
2026-03-31 13:11:00 -0700

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+def PrintHelloWorld():
+    #this function prints the words Hello World!
+    
+    print("Hello World!")
+    
+def main():
+    #this is the main function of the program
+    
+    PrintHelloWorld()
+    
+    
+if __name__=="__main__":
+    main()
