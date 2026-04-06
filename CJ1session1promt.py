f2d7b0ef32f7243e0e3159d18e0787e05e128ee0
Madelynn Bronson <mjbronso@ucsc.edu>
2026-04-06 16:30:40 -0700

    Add files via upload

---
+++ CJ1session1promt.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+print('Madelynn Jane Bronson\nShe/Her\nMy favorite movie is The Batman 2022 because I love comicbooks. My favorite food is pasta because my mom cooked it a lot for me when I was young.')
