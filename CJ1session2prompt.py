21af4c256768e3bd30e6e24659626cce6f6c19da
Madelynn Bronson <mjbronso@ucsc.edu>
2026-04-06 16:48:37 -0700

    Add files via upload
    Write a Python program (.py file) that prints the sum of two floating point numbers, the difference between two integers, and the product of a floating point number and an integer. In each case, have the program print out the data type of the resulting answer.

---
+++ CJ1session2prompt.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 16:33:58 2026
+
+@author: maddyjanebronson
+"""
+
+x = 43.6
+y = 21.1
+
+results = x+y
+
+print("x+y=", results) 
+
+float = 2.4
+print(type(float))
+
+x = 44
+y = 21
+
+results = x-y
+print("x-y=",results)
+
+i = 10
+print(type(i))
+
+x = 43.6
+y = 21
+
+results = x*y
+print("x*y",results)
+
+
+float = 2.4
+print(type(float))
