f6c8388ff5b177d8c0311d56d814b245c3846910
Madelynn Bronson <mjbronso@ucsc.edu>
2026-04-13 16:06:19 -0700

    Session 3 Prompt
    Write a Python program (.py file) that defines a function f(x) that returns x**3 + 8. In the main() function of the program, call f(x) with x = 9 and print the result.  Use an if statement that executes if the result is larger than 27 and prints “YAY!”. Use if __name__ == “__main__”: to setup the program for the command line

---
+++ CJ1session3prompt.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 16:50:43 2026
+
+@author: maddyjanebronson
+"""
+def f(x):
+    f(x) == x**3 +8
+
+x = 9
+
+
+results = 9**3+8
+print(results)
+
+if results > 27:
+    print("YAY!")
+    
+def main():
+    n = 10 
+    
+if __name__ == '__main__':
+        main()
+        
