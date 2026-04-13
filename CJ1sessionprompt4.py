a262b1b2dc9739f14bf0c83afbe409e4b3cda0c9
Madelynn Bronson <mjbronso@ucsc.edu>
2026-04-13 16:38:28 -0700

    Session 4 Prompt
    Write a Python program that declares a class describing your favorite animal. Have the data members of the class represent the following physical parameters of the animal: length of the arms (float), length of the legs (float), number of eyes (int), does it have a tail? (bool), is it furry? (bool). Write an initialization function that sets the values of the data members when an instance of the class is created. Write a member function of the class to print out and describe the data members representing the physical characteristics of the animal.

---
+++ CJ1sessionprompt4.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 13 16:06:39 2026
+
+@author: maddyjanebronson
+"""
+
+#the animal is an octopus 
+print("This is the data of an Octopus!")
+
+#length of arms and legs
+x = float(4.8)
+
+print("The length of an Octopi's limb is 4.8 meters. ")
+print(type(float(4.8)))
+
+# number of eyes
+i = 2
+
+print("Octopi have 2 eyes.")
+print(type(i))
+
+
+#tail&furry
+my_bool = False
+my_bool = False
+
+print("Octopi don't have a tail or are furry.")
+print(type(my_bool))
+
